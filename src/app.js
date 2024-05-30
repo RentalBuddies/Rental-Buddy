4d1a002821047458b4988890ca34bc59066060ed
MOHIT DAVAR <mohitdavar2004@gmail.com>
2024-05-30 10:48:06 +0530

    Intro

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const methodOverride = require("method-override");
 const path = require("path");
+const corsOptions={
+    origin:"http://localhost:8080",
+    methods: ['GET'],
+    allowedHeaders: ['text/css','text/html']
+  }
+
+app.use(cors(corsOptions));
 
 
 app.set("view engine","ejs");
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname,"src")));
 app.set("views",path.join(__dirname,"views"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,ejs}"],
   theme: {
-    extend: {},
+    extend: {
+
+    },
   },
   plugins: [],
 }
